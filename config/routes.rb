23d4ecd4bad4850625fe23a9b672cc93a67c621d
JackMarx <seriousfools@gmail.com>
2018-01-15 17:53:50 -0600

    open to get

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'enterences#guest'
-  post '/open_door', to: 'enterences#open_both', as: 'enterences'
+  get '/open_door', to: 'enterences#open_both', as: 'enterences'
   post '/login', to: 'sessions#create', as: 'login'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
 end
